45076dc1fc14e70746caaee96e4669e7cc7d5793
MOHAMED OURHOUCH <113095806+ourhouchmohamed97@users.noreply.github.com>
2025-11-08 12:31:16 +0000

    feat: add sudo to setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eux
 
-apt update && apt install -y \
+sudo apt update && sudo apt install -y \
   gdb gdb-multiarch git curl wget vim nano netcat-traditional socat \
   nmap gobuster sqlmap john hashcat binwalk steghide exiftool \
   python3-pip python3-venv python3-dev build-essential \
